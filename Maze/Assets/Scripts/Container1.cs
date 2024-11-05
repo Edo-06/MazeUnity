d1238f385e9d7d577ceb776ac7b8766def0f8a5e
ailemamr04@gmail.com <ailemamr04@gmail.com>
2024-11-04 22:11:57 -0500

    adicionando materiales

--- Maze/Assets/Scripts/Container1.cs
+++ Maze/Assets/Scripts/Container1.cs
@@ -7,7 +7,9 @@ using System.Collections.Generic;
 public class Container1 : MonoBehaviour
 {
     public GameObject WallObject;
+    public GameObject ObstacleObject;
     public Maze maze = new Maze();
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +18,6 @@ public class Container1 : MonoBehaviour
             int size = 30;
             maze.Generator(size);
             Print(size);
-
             
             //GameObject wall = Instantiate(WallObject, new Vector3(5, 0, 5), Quaternion.identity);
                // wall.transform.localScale = new Vector3(1, 1, 1);
@@ -34,10 +35,25 @@ public class Container1 : MonoBehaviour
             {
                 for (int j = 0; j < size; j++)
                 {
-                    if(maze.mazee[i, j].category  == Category.wall)
-                    {
-                        GameObject wall = Instantiate(WallObject, new Vector3(i, 1, j), Quaternion.identity);
-                    }
+                switch (maze.mazee[i,j].category)
+                {
+                    case Category.wall:
+                        GameObject wall = Instantiate(WallObject, new Vector3(i, 0.5f, j), Quaternion.identity);
+                        wall.transform.localScale = new Vector3(1, 1, 1); // Ajusta el tamaño si es necesario
+                        break;
+                    case Category.floor:
+                        break;
+                    case Category.obstacle:
+                        GameObject obstacle = Instantiate(ObstacleObject, new Vector3(i, 0, j), Quaternion.identity);
+                        obstacle.transform.localScale = new Vector3(0.5f, 1, 0.5f);
+                        break;
+                    case Category.key:
+                        //Console.Write("🗝️");
+                        break;
+                    case Category.tramp:
+                        //Console.Write("💀");
+                        break;
+                }
                 }
             }
     }
